f2634f2cee8d23d0b1262e551362e52ff2525d5b
iu2fish <wfenng@gmail.com>
2014-11-28 19:45:14 +0800

    modified some files

--- models/todos.js
+++ models/todos.js
@@ -1,7 +1,21 @@
 /**
- * 
+ *
  * @authors iu2fish wfenng@gmail.com
  * @date    2014-11-27 22:19:45
  * @version 0.0.1
  */
 
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+  default: {
+    title:'',
+    completed:false
+  },
+
+  toggle:funtion() {
+    this.save({
+      completed: !this.get('completed')
+    });
+  }
+});
